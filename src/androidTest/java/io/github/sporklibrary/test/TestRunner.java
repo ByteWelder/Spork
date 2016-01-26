6a0e53e6dd8fc651d9618ee939062b8f8193203a
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-01-26 22:35:12 +0100

    increased adb install timeout to 8 minutes

--- src/androidTest/java/io/github/sporklibrary/test/TestRunner.java
+++ src/androidTest/java/io/github/sporklibrary/test/TestRunner.java
@@ -15,10 +15,11 @@ public class TestRunner extends android.support.test.runner.AndroidJUnitRunner
 	{
 		SporkAndroid.initialize();
 
-		// Unlock the device so that the tests can input keystrokes.
+		// Unlock the screen
 		KeyguardManager keyguard = (KeyguardManager) app.getSystemService(Context.KEYGUARD_SERVICE);
 		keyguard.newKeyguardLock(getClass().getSimpleName()).disableKeyguard();
-		// Wake up the screen.
+
+		// Start a wake lock
 		PowerManager power = (PowerManager) app.getSystemService(Context.POWER_SERVICE);
 		mWakeLock = power.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, getClass().getSimpleName());
 		mWakeLock.acquire();
