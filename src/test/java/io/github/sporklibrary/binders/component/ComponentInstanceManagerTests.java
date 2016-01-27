4c3bf6ca983336177e8232eec577c9b02e544cab
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-01-27 21:12:35 +0100

    added ComponentInstanceManager tests

---
+++ src/test/java/io/github/sporklibrary/binders/component/ComponentInstanceManagerTests.java
@@ -0,0 +1,65 @@
+package io.github.sporklibrary.binders.component;
+
+import io.github.sporklibrary.Spork;
+import io.github.sporklibrary.annotations.BindComponent;
+import io.github.sporklibrary.binders.AnnotatedField;
+import io.github.sporklibrary.components.scope.DefaultScopedComponent;
+import io.github.sporklibrary.components.scope.SingletonScopedComponent;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class ComponentInstanceManagerTests
+{
+	// Fields are public for easy test access
+	public static class Parent
+	{
+		@BindComponent
+		public DefaultScopedComponent mDefaultScopedComponent;
+
+		@BindComponent
+		public SingletonScopedComponent mSingletonScopedComponent;
+
+		public Parent()
+		{
+			Spork.bind(this);
+		}
+	}
+
+	@Test
+	public void componentInstanceManager() throws NoSuchFieldException
+	{
+		ComponentInstanceManager manager = new ComponentInstanceManager();
+
+		Parent parent = new Parent();
+
+		Field default_field = parent.getClass().getField("mDefaultScopedComponent");
+		Field singleton_field = parent.getClass().getField("mSingletonScopedComponent");
+
+		BindComponent default_annotation = default_field.getAnnotation(BindComponent.class);
+		BindComponent singelton_annotation = default_field.getAnnotation(BindComponent.class);
+
+		assertNotNull(default_annotation);
+		assertNotNull(singelton_annotation);
+
+		AnnotatedField<BindComponent> default_annotated_field = new AnnotatedField<>(default_annotation, default_field);
+		AnnotatedField<BindComponent> singleton_annotated_field = new AnnotatedField<>(singelton_annotation, singleton_field);
+
+		Object default_instance = manager.getInstance(default_annotated_field, parent);
+		Object singleton_instance = manager.getInstance(singleton_annotated_field, parent);
+		Object singleton_instance_copy = manager.getInstance(singleton_annotated_field, parent);
+
+		assertNotNull(default_instance);
+		assertNotNull(singleton_instance);
+		assertNotNull(singleton_instance_copy);
+
+		assertTrue(DefaultScopedComponent.class.isAssignableFrom(default_instance.getClass()));
+		assertTrue(SingletonScopedComponent.class.isAssignableFrom(singleton_instance.getClass()));
+		assertTrue(SingletonScopedComponent.class.isAssignableFrom(singleton_instance_copy.getClass()));
+
+		assertTrue(singleton_instance == singleton_instance_copy);
+	}
+}
