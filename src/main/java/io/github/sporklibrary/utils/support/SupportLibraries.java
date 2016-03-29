58bdcdb3eee8a8d7a7ac0b7b73355dc237244767
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-03-29 19:35:32 +0200

    implemented a better way to check for optional dependencies

--- src/main/java/io/github/sporklibrary/utils/support/SupportLibraries.java
+++ src/main/java/io/github/sporklibrary/utils/support/SupportLibraries.java
@@ -1,25 +1,48 @@
 package io.github.sporklibrary.utils.support;
 
-import io.github.sporklibrary.annotations.Nullable;
-import io.github.sporklibrary.utils.Classes;
-
 public final class SupportLibraries
 {
-	private static final @Nullable boolean sHasSupportV4;
+	private SupportLibraries()
+	{
+	}
 
-	private static final @Nullable boolean sHasAppCompatV7;
+	private static boolean sHasSupportV4;
 
-	private static final @Nullable boolean sHasRecyclerViewV7;
+	private static boolean sHasAppCompatV7;
+
+	private static boolean sHasRecyclerViewV7;
 
 	static
 	{
-		sHasSupportV4 = Classes.classForNameOrNull("android.support.v4.BuildConfig") != null;
-		sHasAppCompatV7 = Classes.classForNameOrNull("android.support.v7.appcompat.BuildConfig") != null;
-		sHasRecyclerViewV7 = Classes.classForNameOrNull("android.support.v7.widget.RecyclerView") != null;
-	}
+		try
+		{
+			android.support.v4.BuildConfig.class.getName();
+			sHasSupportV4 = true;
+		}
+		catch (NoClassDefFoundError caught)
+		{
+			sHasSupportV4 = false;
+		}
 
-	private SupportLibraries()
-	{
+		try
+		{
+			android.support.v7.appcompat.BuildConfig.class.getName();
+			sHasAppCompatV7 = true;
+		}
+		catch (NoClassDefFoundError caught)
+		{
+			sHasAppCompatV7 = false;
+		}
+
+		try
+		{
+			android.support.v7.widget.RecyclerView.class.getName();
+			sHasRecyclerViewV7 = true;
+		}
+		catch (NoClassDefFoundError caught)
+		{
+			sHasRecyclerViewV7 = false;
+		}
 	}
 
 	public static boolean hasSupportV4()
