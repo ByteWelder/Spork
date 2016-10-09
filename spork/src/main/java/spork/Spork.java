bda549528d90da3c20aa7bbb91c8b044049ef1ea
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-10-09 12:41:08 +0200

    - refactored Spork class to have a constructor that can specify its dependencies directly (mostly for testing) - added more javadoc to Spork class - added Spork class tests - added BinderManager tests

--- spork/src/main/java/spork/Spork.java
+++ spork/src/main/java/spork/Spork.java
@@ -20,10 +20,23 @@ public class Spork {
 	private final BinderRegistry binderRegistry;
 	private final Binder binder;
 
-	private static @Nullable Spork sharedInstance;
+	private static
+	@Nullable
+	Spork sharedInstance;
 
 	/**
-	 * Creates a new instances of Spork with its own BinderManager, Binder and BinderCache.
+	 * Create a new instance of Spork with custom BinderRegistry and Binder implementations.
+	 *
+	 * @param binderRegistry .
+	 * @param binder         .
+	 */
+	public Spork(BinderRegistry binderRegistry, Binder binder) {
+		this.binderRegistry = binderRegistry;
+		this.binder = binder;
+	}
+
+	/**
+	 * Creates a new instances of Spork with the default BinderManager, Binder and BinderCache.
 	 * Spork.sharedInstance() should be used for normal usage.
 	 */
 	public Spork() {
@@ -51,13 +64,11 @@ public class Spork {
 				binderCache.register(typeBinder);
 			}
 		});
-
-		initializeExtension("spork.injection.SporkInjection");
-		initializeExtension("spork.android.SporkAndroid");
 	}
 
 	/**
 	 * Try to initialize a SporkExtension.
+	 *
 	 * @param extensionClassName the SporkExtension class name
 	 */
 	private void initializeExtension(String extensionClassName) {
@@ -65,7 +76,7 @@ public class Spork {
 			Class<?> extensionClass = Class.forName(extensionClassName);
 			Object extensionObject = extensionClass.newInstance();
 			if (extensionObject instanceof spork.interfaces.SporkExtension) {
-				spork.interfaces.SporkExtension extension = (spork.interfaces.SporkExtension)extensionObject;
+				spork.interfaces.SporkExtension extension = (spork.interfaces.SporkExtension) extensionObject;
 				extension.initialize(this);
 			}
 		} catch (ClassNotFoundException e) {
@@ -78,12 +89,19 @@ public class Spork {
 	}
 
 	/**
+	 * The global binding interface to bind annotations for object instances.
+	 *
 	 * @return the interface for binding
 	 */
 	public Binder getBinder() {
 		return binder;
 	}
 
+	/**
+	 * The registry interface to register new annotation binders.
+	 *
+	 * @return the BinderRegistry
+	 */
 	public BinderRegistry getBinderRegistry() {
 		return binderRegistry;
 	}
@@ -91,11 +109,17 @@ public class Spork {
 	// region static methods
 
 	/**
+	 * Get the shard instance of Spork.
+	 * This instance tries to automatically initialize the spork-injection and spork-android libraries when they are available.
+	 *
 	 * @return a shared instance of Spork (creates one if one hasn't been created yet)
 	 */
 	public static Spork sharedInstance() {
 		if (sharedInstance == null) {
 			sharedInstance = new Spork();
+
+			sharedInstance.initializeExtension("spork.injection.SporkInjection");
+			sharedInstance.initializeExtension("spork.android.SporkAndroid");
 		}
 
 		return sharedInstance;
@@ -103,7 +127,8 @@ public class Spork {
 
 	/**
 	 * A shortcut to Spork.sharedInstance().getBinder().bind(object, modules)
-	 * @param object the object to bind
+	 *
+	 * @param object  the object to bind
 	 * @param modules an optional array of non-null module instances
 	 */
 	public static void bind(Object object, Object... modules) {
