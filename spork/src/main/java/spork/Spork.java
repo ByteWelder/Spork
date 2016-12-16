cd565b0f00bfc79ff2e01d3f7028dcd28f339608
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2016-12-16 20:12:06 +0100

    sharedInstance() now implements the single-check idiom

--- spork/src/main/java/spork/Spork.java
+++ spork/src/main/java/spork/Spork.java
@@ -111,14 +111,22 @@ public class Spork {
 	 * Get the shard instance of Spork.
 	 * This instance tries to automatically initialize the spork-injection and spork-android libraries when they are available.
 	 *
+	 * This method is not synchronized as the single-check idiom is applied here:
+	 * If by chance on first call, 2 threads call this method, 2 instances of Spork might be created.
+	 * This is not a problem as one of the 2 instances will just be GC'ed after usage.
+	 *
 	 * @return a shared instance of Spork (creates one if one hasn't been created yet)
 	 */
-	public static synchronized Spork sharedInstance() {
+	public static Spork sharedInstance() {
 		if (sharedInstance == null) {
-			sharedInstance = new Spork();
-
-			sharedInstance.initializeExtension("spork.inject.SporkInject");
-			sharedInstance.initializeExtension("spork.android.SporkAndroid");
+			// Assign locally to avoid double initialization of the same instance
+			Spork spork = new Spork();
+			// Assign to shared instance to make the window smaller that another thread can also
+			// create a new instance and assign it at the same time
+			sharedInstance = spork;
+			// Initialize through the local variable to avoid double initialization by another thread
+			spork.initializeExtension("spork.inject.SporkInject");
+			spork.initializeExtension("spork.android.SporkAndroid");
 		}
 
 		return sharedInstance;
