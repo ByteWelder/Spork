80980a862d20209373da0a242af61dba39d102ab
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2017-04-26 22:25:46 +0200

    created BinderTests

---
+++ spork/src/test/java/spork/internal/BinderTests.java
@@ -0,0 +1,51 @@
+package spork.internal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+public class BinderTests {
+	private BindActionProvider actionProvider;
+	private Binder binder;
+
+	private static class NoInheritanceTarget {
+	}
+
+	private static class InheritanceTarget extends NoInheritanceTarget {
+	}
+
+	@Before
+	public void setup() {
+		Catalog catalog = new Catalog();
+		actionProvider = spy(new BindActionProvider(catalog));
+		binder = new Binder(actionProvider);
+	}
+
+	@Test
+	public void bindObject() {
+		binder.bind(new Object());
+
+		verifyZeroInteractions(actionProvider);
+	}
+
+	@Test
+	public void bindNoInheritance() {
+		binder.bind(new NoInheritanceTarget());
+
+		verify(actionProvider).getBindActions(NoInheritanceTarget.class);
+		verifyNoMoreInteractions(actionProvider);
+	}
+
+	@Test
+	public void bindWithInheritance() {
+		binder.bind(new InheritanceTarget());
+
+		verify(actionProvider).getBindActions(InheritanceTarget.class);
+		verify(actionProvider).getBindActions(NoInheritanceTarget.class);
+		verifyNoMoreInteractions(actionProvider);
+	}
+}
