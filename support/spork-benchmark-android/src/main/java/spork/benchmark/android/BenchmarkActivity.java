a1257ea6ff7b4ef459d49006c9ba30a1f2e26a2b
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2017-05-06 18:07:59 +0200

    Android benchmark now has a simple GUI to trigger it.

--- support/spork-benchmark-android/src/main/java/spork/benchmark/android/BenchmarkActivity.java
+++ support/spork-benchmark-android/src/main/java/spork/benchmark/android/BenchmarkActivity.java
@@ -2,19 +2,54 @@ package spork.benchmark.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
 
+import spork.Spork;
+import spork.android.BindClick;
+import spork.android.BindLayout;
+import spork.android.BindView;
+
+@BindLayout(R.layout.activity_benchmark)
 public class BenchmarkActivity extends Activity {
+	private final Handler handler = new Handler();
 
+	@BindView(R.id.startButton)
+	private Button startButton;
+
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_benchmark);
+		Spork.bind(this);
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+	@BindClick(R.id.startButton)
+	protected void onClickStart() {
+		startBenchmark();
+	}
 
-		spork.benchmark.Application.main(new String[]{});
+	private void startBenchmark() {
+		showBusy(true);
+
+		// Run with delay because otherwise the view is not updated
+		// Run on main thread because background threads get less performance/priority
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				spork.benchmark.Application.main(new String[]{});
+
+				if (isFinishing()) {
+					return;
+				}
+
+				showBusy(false);
+			}
+		}, 100);
+	}
+
+	private void showBusy(boolean busy) {
+		startButton.setEnabled(!busy);
+		startButton.setText(busy ? R.string.busy : R.string.start);
 	}
 }
